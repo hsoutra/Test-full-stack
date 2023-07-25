1713ac86b74551aba4d99fb3b98cea7f5c0f50c7
hsoutra <hsoutra@sqli.com>
2023-07-25 13:15:56 +0100

    setup axios

---
+++ frontend/src/plugins/axios.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+export default {
+  install: (app, options) => {
+    const http = axios.create({
+      baseURL: "http://localhost:8000"
+    })
+    app.provide('http', http)
+  }
+}
--- frontend/src/plugins/index.js
+++ frontend/src/plugins/index.js
@@ -7,12 +7,14 @@
 // Plugins
 import { loadFonts } from './webfontloader'
 import vuetify from './vuetify'
+import axios from './axios'
 import pinia from '../store'
 import router from '../router'
 
 export function registerPlugins (app) {
   loadFonts()
   app
+    .use(axios)
     .use(vuetify)
     .use(router)
     .use(pinia)
